8ecf880f0840ce0e2220805bebdc7f9ea1f1e58f
UsagerLambda <clement.chassem@gmail.com>
2024-09-02 21:05:12 +0200

    fix 8-uppercase.py

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def uppercase(s):
+def uppercase(str):
     string = ""  # Initialise une chaîne vide
-    for i in s:  # 'i' parcourt la chaîne de caractères 's'
+    for i in str:  # 'i' parcourt la chaîne de caractères 's'
         if 'a' <= i <= 'z':  # Vérifie si 'i' est compris entre 'a' et 'z'
             string += chr(ord(i) - 32)  # ord(i) convertit 'i' en ASCII, puis on soustrait 32 pour obtenir l'équivalent majuscule du caractère
             # Ensuite, on reconvertit le code ASCII en caractère (chr) et on stocke ce caractère dans la variable 'string'
