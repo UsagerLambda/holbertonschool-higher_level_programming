879f334595d1817fc799b093db48e4d47fd7addb
UsagerLambda <clement.chassem@gmail.com>
2024-09-02 14:27:10 +0200

    Fix 5-print_string.py

--- python-hello_world/5-print_string.py
+++ python-hello_world/5-print_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Holberton School"
 print(f"{str}{str}{str}")
-print(f"{str[:9]}") # imprime tous les caractères jusqu'au 9ème
+print(f"{str[:9]}\n") # imprime tous les caractères jusqu'au 9ème
