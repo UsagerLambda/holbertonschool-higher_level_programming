dd1dfbc31abef5257ea53270a83eba01a93b1070
UsagerLambda <clement.chassem@gmail.com>
2024-09-03 09:18:53 +0200

    fix Pow

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,16 +1,4 @@
 #!/usr/bin/python3
 def pow(a, b):
-    if b == 0: # si b = 0 retourne 1 car a^0 = 1
-        return 1
-    elif b > 0:
-        result = 1 # initialise 'result' à 1
-        for _ in range(b):
-            result *= a # On multiplie 'result' par 'a', 'b' fois
-        return result
-    else:  # si b est négatif
-        positive_b = -b 
-        result = 1
-        for _ in range(positive_b):
-            result *= a
-        return 1 / result # retourne l'inverse de result
-
+    i = a ** b
+    return i
