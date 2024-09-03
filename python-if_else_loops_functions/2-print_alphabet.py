22bfab740ac6aa723bff2a6b7aa222457ddb693d
UsagerLambda <clement.chassem@gmail.com>
2024-09-03 11:15:47 +0200

    Fix Alphabets

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(97, 123):  # 97 = 'a' & 123 = '{' (z + 1 dans la table ASCII)
-    print("{}".format(chr(i)), end='')  # chr convertit le code ASCII en caractère,
+    print("{}".format(chr(i)), end='')  # chr convertit le code ASCII en char,
     # end='' évite le saut de ligne
