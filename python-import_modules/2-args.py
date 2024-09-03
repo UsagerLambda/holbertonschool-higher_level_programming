ea45f7697d6633c47718eeae10a6f01c9cd6c6b1
UsagerLambda <clement.chassem@gmail.com>
2024-09-03 14:05:14 +0200

    Add 2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,1 +1,14 @@
+#!/usr/bin/python3
+import sys
 
+if __name__ == "__main__":
+    nb_args = len(sys.argv) - 1
+    if nb_args == 0:
+        print(f"{nb_args} arguments.")
+    elif nb_args == 1:
+        print(f"{nb_args} argument:")
+    else:
+        print(f"{nb_args} arguments:")
+    for i, arg in enumerate(sys.argv[1:], start=1):
+        #  argv[1:] ignore la commande & start=1 commence l'indexation à 1
+        print(f"{i:}: {arg}")
