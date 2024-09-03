10727014ab87cd307438b404520f618a0326c134
UsagerLambda <clement.chassem@gmail.com>
2024-09-03 17:43:54 +0200

    Fix P3T0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -2,7 +2,8 @@
 
 from add_0 import add  # Importe la fonction (def) add de add_0
 
+a = 1
+b = 2
+
 if __name__ == "__main__":  # Vérifie si le script est executé directement
-    a = 1
-    b = 2
     print("{} + {} = {}".format(a, b, add(a, b)))
