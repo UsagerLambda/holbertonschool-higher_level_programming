70002c0f47efa40214a54c4547015da2e6daff08
UsagerLambda <clement.chassem@gmail.com>
2024-09-03 11:38:34 +0200

    Fix P2T0

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""
+Classifies random numbers as either negative, zero, or positive.
+
+It uses the `random` module to generate the number and then uses
+conditional statements to check whether the number is less than zero,
+equal to zero, or greater than zero. The result is printed to the console.
+"""
 
 import random
 
