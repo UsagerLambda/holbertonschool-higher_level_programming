24881566f16850ccab4d8dcc161cd4058a5c3124
UsagerLambda <clement.chassem@gmail.com>
2024-09-03 18:01:59 +0200

    remove coms from P3T0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-from add_0 import add  # Importe la fonction (def) add de add_0
+from add_0 import add
 
 a = 1
 b = 2
 
-if __name__ == "__main__":  # Vérifie si le script est executé directement
+if __name__ == "__main__":
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
