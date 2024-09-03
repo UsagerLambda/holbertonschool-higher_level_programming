2371e5c861c60e8625ba1ba4b4d09c0cd419cff5
UsagerLambda <clement.chassem@gmail.com>
2024-09-03 10:33:56 +0200

    Fix Exo 0 & 1

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -3,11 +3,7 @@ from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":  # Vérifie si le script est executé directement
     a = 10
     b = 5
-    result = add(a, b)
-    print(f"{a} + {b} = {result}")
-    result = sub(a, b)
-    print(f"{a} - {b} = {result}")
-    result = mul(a, b)
-    print(f"{a} * {b} = {result}")
-    result = div(a, b)
-    print(f"{a} / {b} = {result}")
+    print(f"{a} + {b} = {add(a, b)}")
+    print(f"{a} - {b} = {sub(a, b)}")
+    print(f"{a} * {b} = {mul(a, b)}")
+    print(f"{a} / {b} = {div(a, b)}")
