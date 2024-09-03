f4d1a995d87df1fafe83e53d3f6e578a159d372a
UsagerLambda <clement.chassem@gmail.com>
2024-09-03 17:40:38 +0200

    add space in P3T0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+
 from add_0 import add  # Importe la fonction (def) add de add_0
+
 if __name__ == "__main__":  # Vérifie si le script est executé directement
     a = 1
     b = 2
