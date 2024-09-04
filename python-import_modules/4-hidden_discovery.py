a9c6fcad210c1038990439d25689be380fee1eee
UsagerLambda <clement.chassem@gmail.com>
2024-09-04 14:31:06 +0000

    fix P3T4

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-if __name__ == '__main__':
-    import hidden_4
+import hidden_4
 
+if __name__ == '__main__':
     for name in dir(hidden_4):
         if name[:2] != '__':
-            print(name)
+            print("{}".format(name))
