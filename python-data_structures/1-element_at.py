98c02c7b0782be0b3a348dc3bc98b3e2964407a5
UsagerLambda <clement.chassem@gmail.com>
2024-09-09 08:27:03 +0200

    Fix P0T1

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    if idx < 0:
-        return "None"
-    elif idx > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return "None"
     else:
-        for i in range(len(my_list)):
-            if my_list[i] == my_list[idx]:
-                return my_list[i]
+        return my_list[idx]
