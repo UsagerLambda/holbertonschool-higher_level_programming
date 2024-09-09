7ddadd521b4e4768dc13e257f9eab4a8ecd67fc3
UsagerLambda <clement.chassem@gmail.com>
2024-09-09 16:51:55 +0200

    Fix P1T12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
+    if not isinstance(roman_string, str):
+        return 0
+
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     result = 0
     length = len(roman_string)
 
-    if len(roman_string) < 0 or roman_string is None:
-        return 0
-
     for i in range(length):
+        if roman_string[i] not in roman:
+            return 0
         value = roman[roman_string[i]]
 
         if (i + 1 < length and
