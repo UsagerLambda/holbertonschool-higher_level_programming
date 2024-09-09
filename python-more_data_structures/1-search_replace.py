d063dfc7b062da4d4cb76b5554ff8629962c81a5
UsagerLambda <clement.chassem@gmail.com>
2024-09-09 13:22:41 +0200

    Add P1T1

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    new_list = my_list.copy()
+    for i in range(len(new_list)):
+        if i == search:
+            new_list[i] = replace
+        else:
+            new_list[i] = my_list[i]
+    return new_list
