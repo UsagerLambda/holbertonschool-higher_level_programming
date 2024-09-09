d7ec80339381caed75ff7c15a941f910affbcaae
UsagerLambda <clement.chassem@gmail.com>
2024-09-09 16:47:02 +0200

    Add P1T12

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    result = 0
+    length = len(roman_string)
+
+    for i in range(length):
+        value = roman[roman_string[i]]
+
+        if (i + 1 < length and
+                roman[roman_string[i]] < roman[roman_string[i + 1]]):
+            result -= value
+        else:
+            result += value
+
+    return result
