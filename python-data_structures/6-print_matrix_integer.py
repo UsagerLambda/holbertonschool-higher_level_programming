34a7b7817ee16665886e92ce12505893fef9eed4
UsagerLambda <clement.chassem@gmail.com>
2024-09-09 10:06:23 +0200

    Fix P0T6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    for lines in matrix:
-        print(" ".join(map(str, lines)))
+    for line in matrix:
+        print(" ".join("{:d}".format(element) for element in line))
