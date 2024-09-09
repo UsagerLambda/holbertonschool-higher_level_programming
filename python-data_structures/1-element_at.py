7d60f0ad2472145b2f7a239904bbd86704f03c6e
UsagerLambda <clement.chassem@gmail.com>
2024-09-09 08:20:34 +0200

    Add P0T1

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    if idx < 0:
+        return "None"
+    elif idx > len(my_list):
+        return "None"
+    else:
+        for i in range(len(my_list)):
+            if my_list[i] == my_list[idx]:
+                return my_list[i]
