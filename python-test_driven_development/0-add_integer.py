7eb1f857c494a7eca20d615b4d3f702e5b4d5371
UsagerLambda <clement.chassem@gmail.com>
2024-09-10 16:27:04 +0200

    Fix P3T0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
 
+"""Module for adding intergers
+
+Check if 'a' and 'b' are integer or float
+if not raise a TypeError
+else if 'a' or 'b' is a float
+convert them into integers and add them """
+
+
 def add_integer(a, b=98):
+
+    """ Adds to numbers ('a' and 'b')
+    after conversion into integers """
+
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     elif not isinstance(b, (int, float)):
