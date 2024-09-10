c36b85a36d8b15b024c8768837cbbc0be5427a8c
UsagerLambda <clement.chassem@gmail.com>
2024-09-10 19:15:25 +0200

    Add P3T1

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+def matrix_divided(matrix, div):
+        if not isinstance(matrix, list):
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+        for line in matrix:
+            if not isinstance(line, list):
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+        for line in matrix:
+            for item in line:
+                if not isinstance(item, (int, float)): 
+                    raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+        first_line = len(matrix[0])
+        for line in matrix:
+            if len(line) != first_line:
+                raise TypeError("Each row of the matrix must have the same size")
+
+        if not isinstance(div, (int, float)):
+            raise TypeError("div must be a number")
+        if div == 0 or div == float('inf') or div != div:
+            raise ZeroDivisionError("division by zero")
+
+        new_matrix = []
+        for line in matrix:
+            new_line = []
+            for item in line:
+                new_line.append(round(item / div, 2))
+            new_matrix.append(new_line)
+
+        return new_matrix
