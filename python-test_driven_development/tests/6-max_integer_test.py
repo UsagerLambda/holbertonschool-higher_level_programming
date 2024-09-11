2b1adf2140e4f9779a20e202a5b3ce089a8ada5c
UsagerLambda <clement.chassem@gmail.com>
2024-09-11 11:03:53 +0200

    Add P3T5

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -2,8 +2,12 @@
 """Unittest for max_integer([..])
 """
 import unittest
+
 max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
-    def max_integer():
-        
+    def test_max_integer_test(self):
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+if __name__ == "__main___":
+    unittest.main()
