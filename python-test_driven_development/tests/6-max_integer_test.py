64056a6cfdf1de0e45457db0295c74d4e7f23c59
UsagerLambda <clement.chassem@gmail.com>
2024-09-11 11:48:12 +0200

    Fix .txt in P3

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -5,6 +5,7 @@ import unittest
 
 max_integer = __import__('6-max_integer').max_integer
 
+
 class TestMaxInteger(unittest.TestCase):
     def test_max_integer_test(self):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
@@ -12,6 +13,7 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([-1, 2, -3, 4, -5]), 4)
         self.assertEqual(max_integer([7]), 7)
         self.assertIsNone(max_integer([]))
+
 
 if __name__ == "__main___":
     unittest.main()
