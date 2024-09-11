045358fe90a8124323bc65e611436a6a1e6a279f
UsagerLambda <clement.chassem@gmail.com>
2024-09-11 08:06:58 +0200

    Add P3T5

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    new_text = ""
+    skip = False
+    for i in range(len(text)):
+        if text[i] == ' ' and skip:
+            continue
+        new_text += text[i]
+        skip = False
+        if text[i] in ".?:":
+            new_text += "\n\n"
+            skip = True
+    print(new_text)
