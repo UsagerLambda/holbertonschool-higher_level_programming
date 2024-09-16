87035cffc6b5a48ec83f5395fcae602263253032
UsagerLambda <clement.chassem@gmail.com>
2024-09-16 15:29:53 +0200

    Fix P1T1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -17,6 +17,8 @@ class Rectangle():
         """
         self.__height = height
         self.__width = width
+        self.height = height
+        self.width = width
 
     @property
     def width(self):
