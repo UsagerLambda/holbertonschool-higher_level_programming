8f224fb4ca25f8d850a7a6ea117ec800e1c5186b
UsagerLambda <clement.chassem@gmail.com>
2024-09-16 15:55:27 +0200

    Fix P1T1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -15,10 +15,8 @@ class Rectangle():
             width (int): The width of the rectangle. Defaults to 0.
             height (int): The height of the rectangle. Defaults to 0.
         """
-        self.__height = height
-        self.__width = width
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
 
     @property
     def width(self):
