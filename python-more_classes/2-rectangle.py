afee67a9721ba17be90d43674ae6980107df220d
UsagerLambda <clement.chassem@gmail.com>
2024-09-16 16:19:44 +0200

    Fix P1T2

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -93,7 +93,17 @@ class Rectangle():
         self.__height = value
 
     def area(self):
+        """
+        Calculate the area of the rectangle.
+
+        Returns: float: The area of the rectangle.
+        """
         return self.width * self.height
 
     def perimeter(self):
+        """
+        Calculate the perimeter of the rectangle.
+
+        Returns: float: The perimeter of the rectangle.
+        """
         return (self.width + self.height) * 2
