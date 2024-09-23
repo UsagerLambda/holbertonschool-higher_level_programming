73d3c904c72c895f89db9c1dfa4efcfbfe7a5a36
UsagerLambda <clement.chassem@gmail.com>
2024-09-23 09:12:58 +0200

    Add P0T1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+class MyList(list):
+    """
+    "MyList" is a class that inherite from the class "list" and add
+    a method to print the list in the ascending order.
+
+    Method:
+        print_sorted(): Print the list in the ascending order.
+    """
+    def print_sorted(self):
+        """ Display the sorted list in ascending order """
+        print(sorted(self))
