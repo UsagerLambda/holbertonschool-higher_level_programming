40980a0118b64b96ebbbe9fb3908ae6263d88686
UsagerLambda <clement.chassem@gmail.com>
2024-09-23 16:27:49 +0200

    Fix P0T10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -54,8 +54,8 @@ class Rectangle(BaseGeometry):
             TypeError: If width or height is not an integer.
             ValueError: If width or height is less than or equal to 0.
         """
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        self.integer_validator('width', width)
+        self.integer_validator('height', height)
         self.__width = width
         self.__height = height
 
