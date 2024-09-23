322fdff59e55f4606777205d210ed265711a1422
UsagerLambda <clement.chassem@gmail.com>
2024-09-23 10:41:38 +0200

    Add P0T2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Function that return "True" if the object is exactly an instance
+of the specified class ; otherwise return "False".
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Check if the object is exactly an instance of the specified class.
+
+    Args:
+        obj: The object to be checked.
+        a_class: The class to compare the type of obj to.
+
+    Returns:
+        True if obj is exactly an instance of a_class, otherwise False.
+    """
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
