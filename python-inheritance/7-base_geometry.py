67a9e7df0841c8c075d1ea0cf9358cae397376ca
UsagerLambda <clement.chassem@gmail.com>
2024-09-23 11:21:13 +0200

    Add P0T7

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+Module defining the BaseGeometry class
+for basic geometric shape operations.
+"""
+
+
+class BaseGeometry():
+    """
+    A class to represent basic geometric shapes.
+
+    Methods
+    -------
+    area():
+        Raises an Exception indicating that the area method is not implemented.
+
+    interger_validator(name: str, value, int)
+        Validate that value is a positive integer, if not raise an Exception
+    """
+    def area(self):
+        """
+        Public method that raise an Exception
+
+        Raises:
+            Exception: "area() is not implemented"
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Validates that value is a positive integer.
+
+        Parameters:
+            name (str): The name of the parameter being validated.
+            value (int): The value to validate.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than or equal to 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError(name + " must be an integer")
+        if value <= 0:
+            raise ValueError(name + " must be greater than 0")
