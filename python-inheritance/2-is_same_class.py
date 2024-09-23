8ae082ff6abf1484cfd6d97cd88482680eb14ea8
UsagerLambda <clement.chassem@gmail.com>
2024-09-23 10:43:13 +0200

    fix P0T2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -16,7 +16,7 @@ def is_same_class(obj, a_class):
     Returns:
         True if obj is exactly an instance of a_class, otherwise False.
     """
-    if type(obj) == a_class:
+    if type(obj) is a_class:
         return True
     else:
         return False
