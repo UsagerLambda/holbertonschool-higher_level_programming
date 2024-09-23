26877557b0e7dc95e3860d3fa86c06fcd08df448
UsagerLambda <clement.chassem@gmail.com>
2024-09-23 17:09:38 +0200

    Create python-abc, add P1T0

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    @abstractmethod
+    def sound(self):
+        pass
+
+class Dog(Animal):
+    def sound(self):
+        super().sound()  # Appelle la méthode 'sound' du parent
+        return "Bark"
+
+class Cat(Animal):
+    def sound(slef):
+        super().sound()  # Appelle la méthode 'sound' du parent
+        return "Meow"
