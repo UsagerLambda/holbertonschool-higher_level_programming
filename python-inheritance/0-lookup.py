3eb5458c3d1fc4c577260000737acb6d7f91a6a3
UsagerLambda <clement.chassem@gmail.com>
2024-09-23 08:21:30 +0200

    Add docstring P0T0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
 
 def lookup(obj):
+    """
+    Returns the list of available attributes and methods of an object.
+
+    Args:
+        obj: The object to inspect.
+
+    Returns:
+        list: A list containing the names of the object's available attributes and methods.
+    """
     return dir(obj)
