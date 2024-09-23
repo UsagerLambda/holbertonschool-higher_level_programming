bce2f5d0c3b98b49ee42f81e0444232a1a985d66
UsagerLambda <clement.chassem@gmail.com>
2024-09-23 11:03:21 +0200

    Add P0T4

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Function that returns True if the object is an instance of a class 
+that inherited (directly or indirectly) from the specified class ;
+otherwise False.
+"""
+
+def inherits_from(obj, a_class):
+    """
+    Returns True if the object is an instance of a class that 
+    inherited (directly or indirectly) from the specified class; 
+    otherwise False.
+
+    Args:
+        obj: The object to check.
+        a_class: type: (bool): The class to compare against.
+
+    Returns:
+        True if the object is an instance of a class that inherited 
+        from a_class (excluding a direct instance of a_class). 
+        Otherwise, False.
+    """
+    if isinstance(obj, a_class) and type(obj) is not a_class:
+        return True
+    else:
+        return False
