fbda997783af0a4285f00c9dc6711f9d02c152b6
UsagerLambda <clement.chassem@gmail.com>
2024-09-23 13:34:33 +0200

    Fix P0T7

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -39,6 +39,6 @@ class BaseGeometry():
             ValueError: If value is less than or equal to 0.
         """
         if not isinstance(value, int):
-            raise TypeError('{} must be an integer'.format(name))
+            raise TypeError(name + " must be an integer")
         if value <= 0:
-            raise ValueError('{} must be greater than 0'.format(name))
+            raise ValueError(name + " must be greater than 0")
