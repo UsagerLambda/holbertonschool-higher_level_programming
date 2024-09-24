c188bc6715928225a97686f622b1d2fde6ff6df4
UsagerLambda <clement.chassem@gmail.com>
2024-09-24 11:09:43 +0200

    Add P1T2

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -4,13 +4,16 @@
 class VerboseList(list):
     def append(self, item):
         super().append(item)
-        print("Added [item] to the list.")
+        print("Added [{}] to the list.".format(item))
 
     def extend(self, iterable):
         super().extend(iterable)
+        print("Extended the list with [{}] items.".format(len(iterable)))
 
     def remove(self, item):
         super().remove(item)
+        print("Removed [{}] from the list.".format(item))
 
-    def pop(self):
-        pass
+    def pop(self, index=-1):
+        item = super().pop(index)
+        print("Popped [{}] from the list.".format(item))
