e744712471ccbb198cb7050d61b49cbac1bc0976
UsagerLambda <clement.chassem@gmail.com>
2024-09-24 14:56:31 +0200

    Fix P0T8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -5,36 +5,7 @@ for basic geometric shape operations.
 """
 
 
-class BaseGeometry:
-    """
-    A class to represent basic geometric shapes.
-    """
-
-    def area(self):
-        """
-        Public method that raise an Exception
-
-        Raises:
-            Exception: "area() is not implemented"
-        """
-        raise Exception('area() is not implemented')
-
-    def integer_validator(self, name, value):
-        """
-        Validates that value is a positive integer.
-
-        Parameters:
-            name (str): The name of the parameter being validated.
-            value (int): The value to validate.
-
-        Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is less than or equal to 0.
-        """
-        if type(value) is not int:
-            raise TypeError('{} must be an integer'.format(name))
-        if value <= 0:
-            raise ValueError('{} must be greater than 0'.format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
