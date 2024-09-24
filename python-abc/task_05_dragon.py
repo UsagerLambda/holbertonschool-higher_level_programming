9955be92774cd47842bf9980e630edf1976fe61a
UsagerLambda <clement.chassem@gmail.com>
2024-09-24 14:14:48 +0200

    Fix P1T5

--- python-abc/task_05_dragon.py
+++ python-abc/task_05_dragon.py
@@ -2,24 +2,24 @@
 
 
 class SwimMixin:
+    """Mixin class that adds swimming functionality."""
 
     def swim(self):
+        """Makes the creature swim."""
         print("The creature swims!")
 
 
 class FlyMixin:
+    """Mixin class that adds flying functionality."""
 
     def fly(self):
+        """Makes the creature fly."""
         print("The creature flies!")
 
 
 class Dragon(SwimMixin, FlyMixin):
+    """Class representing a dragon that can swim and fly."""
 
     def roar(self):
+        """Makes the dragon roar."""
         print("The dragon roars")
-
-
-draco = Dragon()
-draco.swim()
-draco.fly()
-draco.roar()
