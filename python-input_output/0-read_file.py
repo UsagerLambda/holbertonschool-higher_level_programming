647be68215c94a31b2da8e467671441e51155aef
UsagerLambda <clement.chassem@gmail.com>
2024-09-30 08:00:55 +0200

    Fix P0T0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,11 +2,6 @@
 
 
 def read_file(filename=""):
-    """
-    Open and print in UTF-8 the file named by the variable filename.
-
-    Args:
-        filename : the name of the file to open.
-    """
+    """ Open and print in UTF-8 the file named by the variable filename. """
     with open(filename, 'r', encoding="utf-8") as f:
         print(f.read(), end="")
