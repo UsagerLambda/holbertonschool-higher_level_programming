cb2e97d9a4a818202fd9d3b14125d975b2e9c32b
UsagerLambda <clement.chassem@gmail.com>
2024-09-30 19:22:14 +0200

    Fix P0T8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -5,5 +5,6 @@ import json
 
 
 def class_to_json(obj):
-    """Returns the dictionary description of an object for JSON serialization."""
+    """Returns the dictionary description of an object
+    for JSON serialization."""
     return obj.__dict__
