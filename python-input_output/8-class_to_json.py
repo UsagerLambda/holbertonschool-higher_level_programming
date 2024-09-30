9586a7ab8f2e4f8289a75ab128f479c71ea1bc69
UsagerLambda <clement.chassem@gmail.com>
2024-09-30 19:02:53 +0200

    P0T8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""returns the dictionary description"""
+"""Module that contains the class_to_json function."""
+
 import json
 
 
 def class_to_json(obj):
-    """returns the dictionary description"""
+    """Returns the dictionary description of an object for JSON serialization."""
     return obj.__dict__
