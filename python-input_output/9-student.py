9042ce4d1b5ea917d38cc6bbe2fb9641c16dbb0a
UsagerLambda <clement.chassem@gmail.com>
2024-09-30 19:52:11 +0200

    Add P0T9

---
+++ python-input_output/9-student.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+class Student:
+    """Represents a student."""
+    def __init__(self, first_name, last_name, age):
+        """Initializes a new student."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Returns a dictionary representing the student."""
+        return self.__dict__
