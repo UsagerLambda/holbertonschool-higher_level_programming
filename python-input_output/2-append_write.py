fb674cbdf7c4cc83ef5838d9a6ced74a94ac0c6b
UsagerLambda <clement.chassem@gmail.com>
2024-09-30 08:47:07 +0200

    Add P0T2

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""function that write a string to a text file
+and returns the number of characters written"""
+
+
+def append_write(filename="", text=""):
+    """
+    Return the number of char in the file in the variable filename
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        content = f.write(text)
+        return content
