0dfbae84665f542aaf92466c7adc22759d4bca32
UsagerLambda <clement.chassem@gmail.com>
2024-10-01 14:05:30 +0200

    Fix pycodestyle P1T0

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,4 +1,5 @@
-# Importation du module pickle qui permet la sérialisation et la désérialisation d'objets Python.
+# Importation du module pickle qui permet la sérialisation
+# et la désérialisation d'objets Python.
 import pickle
 
 
@@ -8,7 +9,7 @@ def serialize_and_save_to_file(data, filename):
     """
     with open(filename, "wb") as file:
         pickle.dump(data, file)
-    pass
+
 
 def load_and_deserialize(filename):
     """
@@ -17,4 +18,3 @@ def load_and_deserialize(filename):
     with open(filename, "rb") as file:
         loaded_data = pickle.load(file)
     return loaded_data
-    pass
