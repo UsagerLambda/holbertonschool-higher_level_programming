6c4265633fbd461bc6f671e048dc23c996b8b387
UsagerLambda <clement.chassem@gmail.com>
2024-10-01 14:40:44 +0200

    Add P1T1

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+import pickle
+
+
+class CustomObject:
+    """
+    A class to represent a custom object with attributes and methods
+    for serialization and deserialization.
+    """
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        """
+        Prints the attributes of the object in a formatted string.
+        """
+        print("Name: {}\nAge: {}\nIs Student: {}"
+              .format(self.name, self.age, self.is_student))
+
+    def serialize(self, filename):
+        """
+        Serializes the object and saves it to a file using pickle.
+        """
+        with open(filename, "wb") as file:
+            pickle.dump(self, file)
+        
+    @classmethod
+    def deserialize(cls, filename):
+        """
+        Loads a serialized object from a file.
+        """
+        with open(filename, "rb") as file:
+            loaded_data = pickle.load(file)
+        return loaded_data
+
