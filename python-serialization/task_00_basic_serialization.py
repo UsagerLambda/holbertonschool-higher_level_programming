82b50bf3eda52e858984e40ee8a650370c1e91fc
UsagerLambda <clement.chassem@gmail.com>
2024-10-01 14:03:31 +0200

    Add docstrings P1T0

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,12 +1,19 @@
+# Importation du module pickle qui permet la sérialisation et la désérialisation d'objets Python.
 import pickle
 
 
 def serialize_and_save_to_file(data, filename):
+    """
+    Serializes the given data and saves it to a file.
+    """
     with open(filename, "wb") as file:
         pickle.dump(data, file)
     pass
 
 def load_and_deserialize(filename):
+    """
+    Loads and deserializes data from a file.
+    """
     with open(filename, "rb") as file:
         loaded_data = pickle.load(file)
     return loaded_data
