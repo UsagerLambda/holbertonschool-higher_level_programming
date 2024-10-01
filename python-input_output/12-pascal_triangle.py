b41f0c93facd0f258a48a7e4d29c8121a0327b73
UsagerLambda <clement.chassem@gmail.com>
2024-10-01 11:21:20 +0200

    Add P0T12

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return ()
+
+    # initialise une liste avec la première ligne du triangle de Pascal
+    triangle = [[1]]
+
+    # boucle qui va de 1 à n-1 (on ne start pas à 0 car la ligne existe déja)
+    for i in range(1, n):
+        # pour chaque lignes on ajoute le premier élément qui est toujours 1
+        row = [1]
+        # deuxieme boucle qui itere de 1 à i-1
+        for j in range(1, i):
+            # ajoute l'élément correspondant à la valeur des deux éléments
+            # correspondants de la ligne précédente
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        # ajoute 1 car chaque ligne du triangle se termine par 1
+        row.append(1)
+        # on ajoute la ligne fini à triangle
+        triangle.append(row)
+    return triangle
