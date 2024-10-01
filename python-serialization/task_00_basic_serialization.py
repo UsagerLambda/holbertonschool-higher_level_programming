d36969b1c116b7ed8f1e6ffe58a5eee10124f2f6
UsagerLambda <clement.chassem@gmail.com>
2024-10-01 14:06:55 +0200

    Fix pycodestyle P1T0

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -10,7 +10,6 @@ def serialize_and_save_to_file(data, filename):
     with open(filename, "wb") as file:
         pickle.dump(data, file)
 
-
 def load_and_deserialize(filename):
     """
     Loads and deserializes data from a file.
