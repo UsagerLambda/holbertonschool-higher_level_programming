9e4cac45883a3878268f8885c44369bbd3c448b4
Utilisateur Lambda <clement.chassem@gmail.com>
2024-10-17 07:19:36 +0200

    Fix dict users in T5

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -16,7 +16,7 @@ users = {
     "user1": {"username": "user1",
               "password": generate_password_hash("password"), "role": "user"},
     "admin1": {"username": "admin1",
-               "password": generate_password_hash("adminpass"),
+               "password": generate_password_hash("password"),
                "role": "admin"}
 }
 
