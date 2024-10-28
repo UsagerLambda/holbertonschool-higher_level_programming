38cc4d6eb5507f1a6df4f50dcc3300704547e589
UsagerLambda <clement.chassem@gmail.com>
2024-10-28 16:19:10 +0000

    Fix P1T10

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -2,5 +2,5 @@
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 JOIN tv_show_genres
-WHERE tv_shows.id = tv_show_genres.show_id --(selectionne les genre associés aux shows)
+WHERE tv_shows.id = tv_show_genres.show_id -- selectionne les genre associés aux shows
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
