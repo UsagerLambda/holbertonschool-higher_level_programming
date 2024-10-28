b4a3727a57b76a118983ee49ca8634484adf7277
UsagerLambda <clement.chassem@gmail.com>
2024-10-28 14:58:43 +0000

    Fix P1T9

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,5 @@
 -- list all cities in database
-SELECT cities.id, cities.name, states.name FROM cities, states ORDER BY id ASC;
+SELECT cities.id, cities.name, states.name
+FROM cities, states
+WHERE cities.state_id = states.id
+ORDER BY cities.id ASC;
