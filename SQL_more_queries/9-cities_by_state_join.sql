eb0622e5a1ad90fe2b5a7075f75ceae128993397
UsagerLambda <clement.chassem@gmail.com>
2024-10-28 15:01:45 +0000

    Add comments

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- list all cities in database
-SELECT cities.id, cities.name, states.name
-FROM cities, states
-WHERE cities.state_id = states.id
+SELECT cities.id, cities.name, states.name --selectionne les colonnes voulus
+FROM cities, states --ou les colonnes vont être cherchées
+WHERE cities.state_id = states.id --seulement les id dans la table cities correspondant à l'id dans states
 ORDER BY cities.id ASC;
