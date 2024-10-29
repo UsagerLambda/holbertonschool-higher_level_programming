b7c354645db1933177f6e838d0cbfe5ba40e915e
UsagerLambda <clement.chassem@gmail.com>
2024-10-29 15:35:07 +0000

    Add P2T6

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,10 +1,18 @@
-from sqlalchemy import Column, Integer, String
+#!/usr/bin/python3
+"""Define State class"""
+
+from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
+
 
 Base = declarative_base()
 
 
 class State(Base):
+    """SQL State table"""
+
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(50))
+    id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+
+    cities = relationship("City", back_populates="state")
