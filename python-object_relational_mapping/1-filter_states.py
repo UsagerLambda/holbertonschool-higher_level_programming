c21c52ac6c3a049342d994613ca0cd0eb8359c9d
UsagerLambda <clement.chassem@gmail.com>
2024-10-29 13:08:05 +0000

    Fix P2T1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
 
     cursor = conn.cursor()
     cursor.execute(
-        "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+        "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
     results = cursor.fetchall()
 
     for row in results:
