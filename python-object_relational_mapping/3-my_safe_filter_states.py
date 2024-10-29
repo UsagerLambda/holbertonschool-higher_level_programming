174ee38993a4b2aa41b1183e1faa0c85d3b5466d
UsagerLambda <clement.chassem@gmail.com>
2024-10-29 13:30:18 +0000

    Add P2T3

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Module pour afficher toutes les entrées de la table `states`
+triées par `id` dans une base de données MySQL.
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name_searched = sys.argv[4]
+
+    conn = MySQLdb.connect(
+        host='localhost',
+        user=mysql_username,
+        password=mysql_password,
+        db=database_name,
+        port=3306
+        )
+
+    cursor = conn.cursor()
+    requete = (
+        "SELECT * FROM states "
+        "WHERE BINARY name = %s "
+        "ORDER BY states.id ASC")
+    cursor.execute(requete, (state_name_searched,))
+    results = cursor.fetchall()
+
+    for row in results:
+        print(row)
+
+    cursor.close()
+    conn.close()
