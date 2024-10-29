79b991c45b2ccd3d10ce9e9ffba9db6397f1d21a
UsagerLambda <clement.chassem@gmail.com>
2024-10-29 13:03:24 +0000

    Fix P2T1 (pycodestyle + ORDER)

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -21,7 +21,8 @@ if __name__ == "__main__":
         )
 
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id")
+    cursor.execute(
+        "SELECT * FROM statesWHERE name LIKE 'N%' ORDER BY states.id ASC")
     results = cursor.fetchall()
 
     for row in results:
