04257634a01be29200304f1faac488f1d0a1c7e9
UsagerLambda <clement.chassem@gmail.com>
2024-10-29 15:35:58 +0000

    Fix P2T6

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -14,5 +14,3 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-
-    cities = relationship("City", back_populates="state")
