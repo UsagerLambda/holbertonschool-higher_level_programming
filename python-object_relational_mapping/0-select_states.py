0801acbe05a4bce023c368d028af24d54e0c47c4
UsagerLambda <clement.chassem@gmail.com>
2024-10-29 12:56:12 +0000

    Fix pycodestyle P2T0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -12,7 +12,6 @@ if __name__ == "__main__":
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
 
-
     conn = MySQLdb.connect(
         host='localhost',
         user=mysql_username,
