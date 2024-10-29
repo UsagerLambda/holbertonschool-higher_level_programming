09633878137ee100bad6dea9773b76d23be7c5dd
UsagerLambda <clement.chassem@gmail.com>
2024-10-29 12:51:53 +0000

    Fix P2T0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -12,7 +12,13 @@ if __name__ == "__main__":
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
 
-    conn = MySQLdb.connect(host='localhost', user='root', password='root', db='hbtn_0e_0_usa', port=3306)
+    conn = MySQLdb.connect(
+        host='localhost',
+        user='root',
+        password='root',
+        db='hbtn_0e_0_usa',
+        port=3306
+        )
 
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM states ORDER BY states.id")
