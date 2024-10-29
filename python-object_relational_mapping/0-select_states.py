202c722146611b4dd8ced73b8df2bcdadda65fc5
UsagerLambda <clement.chassem@gmail.com>
2024-10-29 12:50:20 +0000

    Fix P2T0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,15 +5,21 @@ triées par `id` dans une base de données MySQL.
 """
 
 import MySQLdb
+import sys
 
+if __name__ == "__main__":
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
 
-conn = MySQLdb.connect(host='localhost', user='root', password='root', db='hbtn_0e_0_usa')
+    conn = MySQLdb.connect(host='localhost', user='root', password='root', db='hbtn_0e_0_usa', port=3306)
 
-cursor = conn.cursor()
-cursor.execute("SELECT * FROM states ORDER BY states.id")
-results = cursor.fetchall()
-for row in results:
-    print(row)
-cursor.close()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY states.id")
+    results = cursor.fetchall()
 
-conn.close()
+    for row in results:
+        print(row)
+
+    cursor.close()
+    conn.close()
