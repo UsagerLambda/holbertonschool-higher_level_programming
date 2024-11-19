63b8e12257809047832b2dc2170873815f23074c
Utilisateur Lambda <clement.chassem@gmail.com>
2024-11-19 18:19:28 +0100

    Fix P2T1

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -1,3 +1,3 @@
 document.getElementById('red_header').onclick = () => {
-    document.querySelector('div').style.color = '#FF0000';
+    document.querySelector('header').style.color = '#FF0000';
 };
