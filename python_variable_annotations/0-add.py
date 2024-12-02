26496a9d1b80a326185d6a9cf4ae07390df902f1
Utilisateur Lambda <clement.chassem@gmail.com>
2024-12-02 13:42:46 +0100

    ADD P1T0

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,4 @@
+# "a: float, b: float" indique que les paramètres reçus doivent être des float,
+# "-> float" indique que la fonction renverra un float.
+def add(a: float, b: float) -> float:
+    return a + b
